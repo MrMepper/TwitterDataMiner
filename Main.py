ddfd4a183d841baa44c4ae16ddb30426cef87f33
Christopher Moore <MrMepper@gmail.com>
2015-05-27 17:33:39 -0500

    Implemented private API keys to file

--- Main.py
+++ Main.py
@@ -3,4 +3,14 @@ __author__ = 'Chris'
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = ''
+#To keep keys secret, left them in a keys.txt file and read them to vars
+with open("keys.txt", "r") as f:
+    APIlist = []
+    for line in f:
+        APIlist.append(line)
+
+consumer_key = APIlist[0]
+consumer_secret = APIlist[1]
+access_token = APIlist[2]
+access_secret = APIlist[3]
+
