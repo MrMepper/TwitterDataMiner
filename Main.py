e5e49d9b94371d75f592dd397b8b4eb30dcbac26
Christopher Moore <MrMepper@gmail.com>
2015-05-27 18:16:06 -0500

    Fixed authentication issue

--- Main.py
+++ Main.py
@@ -2,19 +2,20 @@ __author__ = 'Chris'
 
 import tweepy
 from tweepy import OAuthHandler
+import json
 
 #To keep keys secret, left them in a keys.txt file and read them to vars
-with open("keys.txt", "r") as f:
-    APIlist = []
-    for line in f:
-        APIlist.append(line)
-
-consumer_key = APIlist[0]
-consumer_secret = APIlist[1]
-access_token = APIlist[2]
-access_secret = APIlist[3]
+with open('keys.txt', 'r') as f:
+    consumer_key = f.readline().rstrip()
+    consumer_secret = f.readline().rstrip()
+    access_token = f.readline().rstrip()
+    access_secret = f.readline().rstrip()
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    print(status.text)
