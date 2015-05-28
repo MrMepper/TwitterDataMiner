9be1ea96872e9d09ccc7ed8cbd3191f04e8e0099
Christopher Moore <MrMepper@gmail.com>
2015-05-28 00:37:29 -0500

    Added MyListener class, input to twitter stream.
    creates python.json file that stores posts from the #python twitter page

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ __author__ = 'Chris'
 
 import tweepy
 from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import json
 
 #To keep keys secret, left them in a keys.txt file and read them to vars
@@ -16,6 +18,20 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
+class MyListener(StreamListener):
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
+
