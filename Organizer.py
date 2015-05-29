524585bccd71679c1af95db660bbf5174d2aec67
Christopher Moore <MrMepper@gmail.com>
2015-05-28 19:47:48 -0500

    Added Organizer object

---
+++ Organizer.py
@@ -0,0 +1,17 @@
+__author__ = 'Chris'
+
+import json
+
+
+class Organizer(object):
+
+    filename = ""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def organize(self):
+        with open (filename, "r") as f:
+            line = f.readline()
+            tweet = json.loads(line)
+            print(json.dumps(tweet, indent = 4))
