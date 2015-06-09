b722a53848856a4696bc4b6bad15700c0c38014d
Christopher Moore <MrMepper@gmail.com>
2015-06-09 15:06:38 -0500

    Implemented stopwords. Download of stopwords from NLTK is needed.

--- Term-Counter.py
+++ Term-Counter.py
@@ -6,10 +6,13 @@ import json
 from collections import Counter
 import Tokenizer
 from nltk.corpus import stopwords
+from nltk import download
 import string
 
+download()
 fname = input("Enter file name")
 token = Tokenizer
+
 punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + ['rt', 'via']
 
@@ -18,7 +21,7 @@ with open(fname, 'r') as f:
     count_all = Counter()
     for line in f:
         tweet = json.loads(line)
-        terms_all = [term for term in token.preprocess(tweet['text'])]
+        terms_all = [term for term in token.preprocess(tweet['text']) if term not in stop]
         count_all.update(terms_all)
     print(count_all.most_common(5))
 
